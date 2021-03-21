1393fc29d2f09809593b2a3212da862036fc357b
muzna <hallomuze@gmail.com>
2021-03-21 11:58:28 +0900

    Initial Commit

---
+++ Localize101/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Localize101
+//
+//  Created by muzna on 2021/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
